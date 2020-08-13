05d593c80fb2fd55ed7c53d5dbbc0306e1d8cbdb
Irvin Cabal <irvin.cabalmoreira@claro.com.ar>
2020-08-13 01:04:26 -0300

    Conversor y Condicionales

--- Conversor.py
+++ Conversor.py
@@ -1,2 +1,7 @@
 pesos = input ("¿Cuantos pesos argetinos tienes?")
-peso = float(pesos)
+pesos = float(pesos)
+valor_dolar = 107
+dolares = pesos / valor_dolar
+dolares = round(dolares,2)
+dolares = str(dolares)
+print("Tienes $" + dolares + " dolares")
