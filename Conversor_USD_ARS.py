50c9c8a7b0078abe34bd1bb751bec25a33b4605d
Irvin Cabal <irvin.cabalmoreira@claro.com.ar>
2020-08-13 01:07:13 -0300

    Conversor ARG y Condicionales

---
+++ Conversor_USD_ARS.py
@@ -0,0 +1,7 @@
+dolares = input ("¿Cuantos doalres tienes?")
+dolares = float(dolares)
+valor_dolar = 100
+pesos = dolares * valor_dolar
+pesos = float(pesos)
+pesos = str(pesos)
+print("Tienes $ARG" + pesos + " pesos")
